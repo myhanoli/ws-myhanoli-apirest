ea32dd6ab16b1ab33e59b104558b70493b7977cb
myhanoli.com <myhanoli@hotmail.com>
2020-06-15 00:52:47 -0500

    Se modifico controlador

--- src/main/java/com/myhanoli/apirest/controllers/EmpleadoRestController.java
+++ src/main/java/com/myhanoli/apirest/controllers/EmpleadoRestController.java
@@ -1,14 +1,13 @@
 package com.myhanoli.apirest.controllers;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.myhanoli.apirest.models.entity.Empleado;
-import com.myhanoli.apirest.models.services.IEmpleadoServiceImpl;
+import com.myhanoli.apirest.models.services.IEmpleadoService;
+
 
 @RestController
 @RequestMapping("/api")
@@ -16,13 +15,15 @@ public class EmpleadoRestController {
 
 	
 	@Autowired
-	private IEmpleadoServiceImpl empleadoService;
+	private IEmpleadoService empleadoService;
 	
-	@GetMapping("/listar")
+	@GetMapping("/empleados")
 	public List<Empleado> getEmpleados(){
 		
 		return empleadoService.findAll();
 		
 	}
+	
+	
 	
 }
